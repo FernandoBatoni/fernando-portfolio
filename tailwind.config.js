e4f09b97b5cb4ee559d9fd2a79660088af9d34fc
FernandoBatoni <nandobatoni22@gmail.com>
2023-09-13 17:29:07 -0300

    ref: refactoring to better suit front-end guidelines, added tailwind and less, react etc for future developments if needed and future projects as a template

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{ts,tsx.astro}',
+    './components/**/*.{ts,tsx.astro}',
+    './app/**/*.{ts,tsx.astro}',
+    './src/**/*.{ts,tsx.astro}',
+  ],
+  plugins: [require("tailwindcss-animate")],
+}
